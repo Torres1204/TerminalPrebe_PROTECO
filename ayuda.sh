e3fb30c881a299a20dc016abcc0de7c37c7add53
nataliamr20 <natalia.medina@ingenieria.unam.edu>
2023-09-17 00:16:08 -0600

    [FEATURE] se  creó la lista de ayuda

---
+++ ayuda.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "Nombre del equipo: $(uname -n)\n"
+echo "  Bienvenido $USER"
+echo "-----------------------------------------------------------------------------"
+echo "Lista de comandos disponibles..."
+echo "-----------------------------------------------------------------------------"
+echo "    Comando    | \t Descripcion "
+echo "    ayuda      | Muestra los comandos disponibles y su funcionalidad "
+echo "    buscar     | Busca un archivo en el sistema "
+echo "    créditos   | Muestra los nombres de los desarrolladores "
+echo "    infosis    | Muestra información del sistema "
+echo "      mp3      | Es un reproductor de musica "
+echo "   fechayhora  | Muestra la zona, fecha y hora "
+echo "    juego      | Muestra y ejecuta el juego programado "
+echo "     salir     | Sale de la terminal prebe "
+
