d2ed3c49ae11fbf30b6e7209fad243c23b452f5c
“Ángel <143279596+Torres1204@users.noreply.github.com>
2023-09-16 22:13:10 -0600

    [FEATURE] se creo el contenido del comando fechayhora

---
+++ fechayhora.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+fecha_hora=$(TZ="America/Mexico_City" awk 'BEGIN {split(strftime("%d %m %Y %H %M %S"), time); print time[1] "-" time[2] "-" time[3] " " time[4] ":" time[5] ":" time[6]}')
+
+echo "Fecha y hora actual: $fecha_hora"
+
+
+
+
+
+
+
