8985bb5a52db18fc3e50d0c7ef31f7817d963889
nataliamr20 <natalia.medina@ingenieria.unam.edu>
2023-09-19 22:11:45 -0600

    [FEATURE] avance creditos

--- creditos.sh
+++ creditos.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+#Dar formatooo
+echo " "
+echo "------ Este proyecto fue realizado por ------"
+echo ""
+echo ""
+echo "Flores Torres Ángel Joel"
+echo "Medina Rubalcava Natalia Michell"
