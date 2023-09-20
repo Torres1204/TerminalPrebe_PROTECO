396644cfd2b635a7a88c5c9cb2bf78ca1eb8aceb
nataliamr20 <natalia.medina@ingenieria.unam.edu>
2023-09-19 22:05:09 -0600

    actualizado

--- prueba.sh
+++ prueba.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-    ##usuario=$USER #ahi se guarda el usuario que está en el sistema, y con el que se va a comparar
-    contrasena=aaa #contraseña desencriptada
     
     echo "----- Bienvenido -----"
     CONT=3
@@ -12,7 +10,7 @@
 
         echo "Ingrese su contraseña: "
         read Contra
-        echo $contra | su $nomUsuario -c sh prueba.sh; 
+        echo $Contra | su $nomUsuario -c sh prueba.sh; 
 
         
         #Si el comando falla vuelve  a preguntar, si no ya se va al menú
