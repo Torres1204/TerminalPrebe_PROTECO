a0b126f6b8fe74b2f6975e9085fc7d6af1f0701f
“Ángel <143279596+Torres1204@users.noreply.github.com>
2023-09-22 09:48:59 -0600

    [FEATURE] se modifico el formato de impresión

--- fechayhora.sh
+++ fechayhora.sh
@@ -2,7 +2,8 @@
 
 fecha_hora=$(TZ="America/Mexico_City" awk 'BEGIN {split(strftime("%d %m %Y %H %M %S"), time); print time[1] "-" time[2] "-" time[3] " " time[4] ":" time[5] ":" time[6]}')
 
-echo "Fecha y hora actual: $fecha_hora"
+printf "\n"
+echo -e "Fecha y hora actual: $fecha_hora \n"
 
 
 
