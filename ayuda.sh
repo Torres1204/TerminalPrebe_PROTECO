c92aba416836855c53300ead832e1a721fced4e2
“Ángel <143279596+Torres1204@users.noreply.github.com>
2023-09-22 08:16:09 -0600

    [FUTURE] se mejoro el formato de impresion

--- ayuda.sh
+++ ayuda.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-
+printf "\nPresiona enter para continuar"
+read
+clear
+printf "\n\n" 
 echo -e "Nombre del equipo: $(uname -n)\n"
-echo "  Bienvenido $USER"
-echo "-----------------------------------------------------------------------------"
-echo "Lista de comandos disponibles..."
+printf "\t\t\tBienvenido $USER \n"
 echo "-----------------------------------------------------------------------------"
 echo "    Comando    |         Descripcion "
-echo "    ayuda      | Muestra los comandos disponibles y su funcionalidad "
-echo "    buscar     | Busca un archivo en el sistema "
+echo "---------------|-------------------------------------------------------------"
+echo "     ayuda     | Muestra los comandos disponibles y su funcionalidad "
+echo "     buscar    | Busca un archivo en el sistema "
 echo "    creditos   | Muestra los nombres de los desarrolladores "
 echo "    infosis    | Muestra información del sistema "
-echo "      mp3      | Es un reproductor de musica "
-echo "   fechayhora  | Muestra la zona, fecha y hora "
-echo "    juego      | Muestra y ejecuta el juego programado "
+echo "      mp3      | Ejecuta un reproductor de musica "
+echo "   fechayhora  | Muestra la fecha y hora "
+echo "     juego     | Muestra y ejecuta el juego programado "
 echo "     salir     | Sale de la terminal prebe "
 
