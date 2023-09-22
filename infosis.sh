c7edd946eb92024e438b0f6c94d7db32b6e9222b
“Ángel <143279596+Torres1204@users.noreply.github.com>
2023-09-22 09:45:57 -0600

    [FEATURE] se modifico el formato de impresión

--- infosis.sh
+++ infosis.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+Glig='\e[1;32m' # Verde claro
+reset="\033[0m"
 
-echo "---INFORMACIÓN DEL SISTEMA---"
+printf "\n $Glig ---INFORMACIÓN DEL SISTEMA---\n $reset"
 
-echo -e "Nombre del equipo: $(uname -n)\n"
+printf "\t\nNombre del equipo: $(uname -n)\n"
 
-echo -e "Sistema operativo: $(uname -o)\n"
+printf "\t\nSistema operativo: $(uname -o)\n"
 
-echo -e "Versión del kernel: $(uname -v)\n"
+printf "\t\nVersión del kernel: $(uname -v)\n"
 
-echo -e "Información de la memoria: $(uname -i)\n"
+printf "\t\nInformación de la memoria: $(uname -i)\n"
 
-echo -e "Memoria RAM: $(free -h)\n"
+printf "\t\nMemoria RAM: $(free -h)\n"
 
-echo -e "Arquitectura del sistema: $(uname -m)\n"
+printf "\t\nArquitectura del sistema: $(uname -m)\n"
 
-echo -e "Version del SO: $(uname -r)\n"
+printf "\t\nVersion del SO: $(uname -r)\n"
 
-echo -e "Tipo de procesador: $(uname -p)\n"
+printf "\t\nTipo de procesador: $(uname -p)\n\n"
